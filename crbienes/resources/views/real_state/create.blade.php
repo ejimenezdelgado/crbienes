21b40ed00df595d09e25e105eb14ef93e5ae3f63
Efren Jimenez <efjimenez@itcr.ac.cr>
2016-12-06 19:19:25 -0600

    New views

--- crbienes/resources/views/real_state/create.blade.php
+++ crbienes/resources/views/real_state/create.blade.php
@@ -1,9 +1,33 @@
+@extends('layout.master')
 
+@section('title')
+Bienes raíces
+@stop
 
+@section('menu')
+    <li><a href="/">Inicio</a></li>
+    <li><a href="#">Buscar</a></li>
+    <li><a href="/contact">Contactanos</a></li>
+@stop
+
+@section('content')
 {{Form::open(array('real_state', 'method' => 'post'))}}
 {{ csrf_field() }}
 
 <div class="form-group">
+	<label for="name" class="col-md-4 control-label">Nombre</label>
+	<div class="col-md-6">
 	{{ Form::input('text','name',null,['class' => 'form-control']) }}
+	</div>
 </div>
+<div class="form-group">
+    <label for="description" class="col-md-4 control-label">Descripción</label>
+
+    <div class="col-md-6">
+        <input id="description" type="text" class="form-control" name="description" value="{{old('description')}}" required autofocus>
+    </div>
+</div>
+
 {{ Form::close() }}
+
+@stop
--- crbienes/routes/web.php
+++ crbienes/routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', function () {
+	return "hola mundo";
     return view('welcome');
 });
 
